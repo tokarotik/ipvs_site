ec28db59427d46f3bee56f1a6eff0b6dd40affcf
ToKarotik <65163247+tokarotik@users.noreply.github.com>
2026-01-08 21:48:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
+import os
 from flask import Flask, send_from_directory, send_file
 
 app = Flask(__name__)
+rootdir = os.getcwd().replace('\\', '/') + '/'
 
 @app.route("/")
 def home():
-    return open("site/index.html", encoding="utf-8").read()
+    return open(rootdir +"site/index.html", encoding="utf-8").read()
 
 @app.route("/license")
 def license():
-    return f"<pre>{ open('LICENSE.txt', encoding='utf-8').read() }</pre>"
+    return f"<pre>{ open(rootdir + 'LICENSE.txt', encoding='utf-8').read() }</pre>"
 
 @app.route("/<namepage>")
 def page(namepage=None):
     if namepage is None: return home()
     try:
-        return open(f"site/pages/{namepage}.html", encoding="utf-8").read()
+        return open(rootdir + f"site/pages/{namepage}.html", encoding="utf-8").read()
     except FileNotFoundError:
         return page_not_found(404)
+    except Exception:
+        return "000", 500
     
 @app.route("/scripts/<namepage>")
 def scripts(namepage=None):
     if namepage is None: return page_not_found(404)
     try:
-        return open(f"site/scripts/{namepage}", encoding="utf-8").read()
+        return open(rootdir + f"site/scripts/{namepage}", encoding="utf-8").read()
     except FileNotFoundError:
         return page_not_found(404)
+    except Exception:
+        return "000", 500
     
 @app.route("/scripts/libs/<namepage>")
 def libs(namepage=None):
@@ -37,13 +43,15 @@ def styles(namepage=None, sheet=None):
     if sheet is None:
         sheet = namepage
     try:
-        return send_from_directory(f"site/pages/{namepage.replace('.css', '')}", sheet)
+        return send_from_directory(rootdir + f"site/pages/{namepage.replace('.css', '')}", sheet)
     except FileNotFoundError:
         return page_not_found(404)
+    except Exception:
+        return "000", 500
     
 @app.errorhandler(404)
 def page_not_found(e):
-    return open("site/404.html", encoding="utf-8").read(), 404
+    return open(rootdir + "site/404.html", encoding="utf-8").read(), 404
 
 
-app.run(host="2a02:587:4d09:b400:4b07:bf9c:8f95:e226", port=8000)
+app.run(host="::", port=8000)
